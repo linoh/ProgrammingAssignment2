c2990f4a13ff371113c12d95b1a438b9bfa22088
linoh <harry.lino@bigpond.com>
2015-03-22 00:21:23 +1000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Theses functions below create an object that captured and stores a matrix and cache's its inverse 
+## The makeCachematrix create a specific matrix tha have capability that;-
+## set the matrix value
+## get the matrix value
+## set the inverse value
+## get the inverse value
 
 makeCacheMatrix <- function(x = matrix()) {
+        inverse <- NULL                                 ## set NULL to inverse
+        sMatrix <- function (y) {
+                x <<- y                                 ## Asigned x to the matrix
+                inverse <<- NULL
+        }
+        gMatrix <-function() x                          ## returning matrix x
+        sInverse <-function(solve) inverse <<-solve     ## cache inverse value        
+        gInverse <-function() inverse                   ## returning inverse
+        list(sMatrix = sMatrix, gMatrix = gMatrix,
+                sInverse = sInverse,
+                gInverse = gInverse)
 
-}
 
-
-## Write a short comment describing this function
+## This function calculate the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+        inverse <-x$gInverse                            ## getting inverse
+        if(!is.null(inverse)) {                         ## check if inverse value is NULL
+                message("getting cache data")           ## if true then display message
+                return(inverse)
+        }
+        data <- x$gMatrix()                             ## getting matrix
+        inverse <- solve(data, . . .)                   ## compute inverse        
+        x$sInverse(inverse)                             ## cache inverse
+        inverse                                         ## returning inverse value
+}        
+
